4ba1e1ce61d3390df62be63bc6d9819f1b84d083
MkshSahani <msco18333@gmail.com>
2020-10-01 22:51:38 +0530

    Updated string to int

--- String/stringToInt.cpp
+++ String/stringToInt.cpp
@@ -3,6 +3,14 @@
 
 using namespace std; 
 
+int abs(int x) {
+    if(x < 0) {
+        return -1 * x; 
+    } else {
+        return x; 
+    }
+}
+
 int stringToInt(string data) {
     int start = 0, index = 0, temp = 0, result = 0;
     bool isNegative = false; 
@@ -17,9 +25,29 @@ int stringToInt(string data) {
     return (isNegative == true) ? -1 * result : result; 
 }
 
+string intToString(int data) {
+    string result;
+    bool isNegative = false;
+    if(data < 0) {
+        isNegative = true; 
+    }
+    int temp = 0; 
+    while(data) {
+        temp = data % 10;
+        result += abs(temp) + '0'; 
+        data /= 10; 
+    }
+    if(isNegative) {
+        result += '-'; 
+    }
+
+    return {result.rbegin(), result.rend()}; 
+}
+
 int main(int argc, char* argv[]) {
     string data; 
     cin >> data;
-    cout << stringToInt(data) << endl; 
-    return 0; 
+    cout << stringToInt(data) << endl;
+    cout << intToString(stringToInt(data)) << endl; 
+    return 0;
 }
