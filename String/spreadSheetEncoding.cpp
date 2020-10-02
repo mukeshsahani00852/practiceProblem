09e09f72ccb0f8a9d347f1d87944ba1b164eeb82
MkshSahani <msco18333@gmail.com>
2020-10-02 23:28:49 +0530

    Decoding updated

--- String/spreadSheetEncoding.cpp
+++ String/spreadSheetEncoding.cpp
@@ -3,11 +3,45 @@
 
 using namespace std; 
 
-int Decode(string str) {
+int Decode(string str) 
+{
+    int decode = 0, temp = 0; 
+    for (int i = 0; i < str.length(); ++i)
+    {
+        temp = str[i] - 'A';
+        decode = decode * 26 + temp + 1; 
+    }
 
+    return decode; 
 }
 
-int main(int argc, char* argv[]) {
+string Encode(int value) 
+{
+    string result;
+    int temp = 0;
+    char p = 0; 
+    while (value)
+    {
+        temp = value % 26; 
+        if(temp == 0) 
+        {
+            temp = 26; 
+        }
 
-    return 0; 
+        p = temp + 'A'; 
+        result += p;
+        // temp = temp / 26;
+        value /= 26; 
+    }
+
+    return {result.rbegin(), result.rend()}; 
+}
+
+int main(int argc, char* argv[]) 
+{
+    string data; 
+    cin >> data;
+    cout << Decode(data) << endl;
+    cout << Encode(Decode(data)) << endl;
+    return 0;
 }
