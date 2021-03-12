f847a1ead9e4f654bdb7ebbfd6cf1979a9aefbd3
MkshSahani <msco18333@gmail.com>
2021-03-12 11:46:23 +0530

    python added

--- java/datastructure/datastructure/arrayList/ArrayStringProblem.java
+++ java/datastructure/datastructure/arrayList/ArrayStringProblem.java
@@ -15,10 +15,34 @@ public class ArrayStringProblem {
             if(char_set[data.charAt(i)]) {
                 return false; 
             }  
-            char_set[i] = true; 
+            char_set[data.charAt(i)] = true; 
         }
         return true; 
     }
+
+    /**
+     * @function : isPermutation.
+     * @name : isPermutation_version_1 
+     * @working : in this implementation i have sorted both string than compare them if both are equal 
+     * than they are permutation of each other. 
+     */
+
+    public static boolean isPermutation_version1(String str1, String str2) {
+        if(str1.length() != str2.length()) {
+            return false; 
+        } else {
+            return stringSort(str1).equals(stringSort(str2)); 
+        }
+    }
+
+    public static String stringSort(String str) {
+        char[] char_array = str.toCharArray(); 
+        java.util.Arrays.sort(char_array); 
+        return new String(char_array); 
+    }
+
+
+    
 
 
 }
--- java/datastructure/datastructure/testing/arrayStringTest.java
+++ java/datastructure/datastructure/testing/arrayStringTest.java
@@ -7,17 +7,14 @@ public class arrayStringTest {
     public static void main(String[] args) {
         
 
-        /**
-         * @problem : determine wheter string contain unique character or not. 
-         */
-
         Scanner sc = new Scanner(System.in); 
 
-        String data = sc.next(); 
-        if(ArrayStringProblem.isUnique(data)) {
-            System.out.println("All character are not unique !"); 
+        String str1 = sc.next(), str2 = sc.next();
+        
+        if(ArrayStringProblem.isPermutation_version1(str1, str2)) {
+            System.out.println(str1 + " " + str2 + " are permutation of each other"); 
         } else {
-            
+            System.out.println(str1 + " " + str2 + " are not permutation of each other"); 
         }
 
         sc.close(); // close scanner. 
