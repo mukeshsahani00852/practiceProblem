ef94ede9db34cc3e9ffba927d5ebdb28a3ff6b69
MkshSahani <msco18333@gmail.com>
2021-07-10 21:23:12 +0530

    Queue using Stack in python

--- Stack/StackProblems.py
+++ Stack/StackProblems.py
@@ -1,4 +1,32 @@
 # -------------------------stack problems----------------------------------- 
-
+from Stack import Stack 
 # --------------------------problem : 1 ------------------------------------ 
+
+# implement MyQueue using Stack. 
+class MyQueue: 
+
+    def __init__(self): 
+        self.main_stack = Stack()
+
+    
+    def enQueue(self, ele): 
+        self.main_stack.push(ele)
+
+    def deQueue(self): 
+        if self.main_stack.isEmpty(): 
+            return None 
+        else: 
+            aux_stack = Stack()
+            while self.main_stack.isEmpty(): 
+                aux_stack.push(self.main_stack.pop())
+            data = aux_stack.pop()
+
+            while aux_stack.isEmpty(): 
+                self.main_stack.push(aux_stack.pop()) 
+
+        return data 
+    
+    def isEmpty(self): 
+        return self.main_stack.isEmpty()
+    
 
